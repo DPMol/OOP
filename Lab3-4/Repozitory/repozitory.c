3d0f0d56810e82ed59b6e461eb9b876f56391fe2
12den <12denismoldovan@gmail.com>
2022-03-07 16:05:25 +0200

    malloc fix

--- Lab3-4/Repozitory/repozitory.c
+++ Lab3-4/Repozitory/repozitory.c
@@ -3,13 +3,13 @@
 #include "string.h"
 
 typedef struct{
-    type_participant **list;
+    void **list;
     int size;
     int capacity;
 
 }repo;
 
-repo array;
+repo *array;
 
 void repo_default_values(){
     type_participant *participant = (type_participant *)malloc(sizeof(type_participant));
@@ -49,51 +49,52 @@ void repo_default_values(){
 }
 
 void repo_initialization(){
-    array.size = 0;
-    array.capacity = 20;
-    array.list = (type_participant **)malloc(sizeof(type_participant*));
+    array = (repo *)malloc(sizeof(repo));
+    array->size = 0;
+    array->capacity = 60;
+    array->list = malloc(array->capacity* sizeof(type_participant*));
     repo_default_values();
 }
 
 void repo_destructor(){
-    for(int i = 0; i < array.size; i++)
-        free(array.list[i]);
-    free (array.list);
+    for(int i = 0; i < array->size; i++)
+        free(array->list[i]);
+    free (array->list);
 }
 
 void repo_realloc(int newcapacity){
-    type_participant **newlist = (type_participant **)malloc(sizeof(type_participant*));
-    for(int i = 0; i < array.size; i++){
-        newlist[i] = array.list[i];
+    void **newlist = malloc(newcapacity* sizeof(type_participant*));
+    for(int i = 0; i < array->size; i++){
+        newlist[i] = array->list[i];
     }
-    free(array.list);
-    array.list = newlist;
-    array.capacity = newcapacity;
+    free(array->list);
+    array->list = newlist;
+    array->capacity = newcapacity;
 }
 
 int repo_delete(int id){
-    if(id >= array.size)
+    if(id >= array->size)
         return 0;
-    array.size--;
-    free(array.list[id]);
-    for(int i = id; i < array.size; i++){
-        array.list[i] = array.list[i+1];}
+    array->size--;
+    free(array->list[id]);
+    for(int i = id; i < array->size; i++){
+        array->list[i] = array->list[i+1];}
     return 1;
 }
 
 void repo_add(type_participant *participant){
-    if(array.size >= array.capacity){
-        repo_realloc(2 * array.size);
+    if(array->size >= array->capacity){
+        repo_realloc(2 * array->size);
     }
-    array.list[array.size++] = participant;
+    array->list[array->size++] = participant;
 }
 
 type_participant * repo_get_by_id(int id){
-    if(id >= array.size)
+    if(id >= array->size)
         return NULL;
-    return array.list[id];
+    return array->list[id];
 }
 
 int repo_get_size(){
-    return array.size;
+    return array->size;
 }
